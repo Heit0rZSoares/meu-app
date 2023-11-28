93e29d2f8d6e7237931f4c962cea16dbab09b83d
Heit0rZSoares <152338791+Heit0rZSoares@users.noreply.github.com>
2023-11-28 19:43:16 -0300

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -21,9 +21,6 @@ const Home = () => {
           <li>
             <Link to={"/Atividade04"}>Calculadora</Link>
           </li>
-          <li>
-            <Link to={"/Atividade05"}>Jogo da Memoria</Link>
-          </li>
         </ul>
       </nav>
     </div>
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import Atividade01 from "./Atividade01/Atividade01";
 import Atividade02 from "./Atividade02/Atividade02";
 import Atividade03 from "./Atividade03/Atividade03";
 import Atividade04 from "./Atividade04/Atividade04";
-import Atividade05 from "./Atividade05/Atividade05";
+
 
 
 export default function routes() {
@@ -17,7 +17,6 @@ export default function routes() {
                 <Route path="/Atividade02" element={<Atividade02/>}/>
                 <Route path="/Atividade03" element={<Atividade03/>}/>
                 <Route path="/Atividade04" element={<Atividade04/>}/>
-                <Route path="/Atividade05" element={<Atividade05/>}/>
             </Routes>
         </BrowserRouter>
     );
