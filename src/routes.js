b0c68a249e6a828540363c79b08578be3f2475fe
Heit0rZSoares <152338791+Heit0rZSoares@users.noreply.github.com>
2023-11-28 19:23:03 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import Atividade01 from "./Atividade01/Atividade01";
 import Atividade02 from "./Atividade02/Atividade02";
 import Atividade03 from "./Atividade03/Atividade03";
 import Atividade04 from "./Atividade04/Atividade04";
-import Atividade05 from "./Atividade05/Atividade05";
+
 
 
 export default function routes() {
